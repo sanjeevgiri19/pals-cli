a6bd4ebce959541136f1ec6a9b734864cad25a91
Sanjeev Giri <girisanjib191@gmail.com>
2026-04-03 23:30:56 +0545

    feat: add tool integration and selection functionality for AI interactions

--- server/src/cli/commands/ai/wakeUp.js
+++ server/src/cli/commands/ai/wakeUp.js
@@ -5,6 +5,7 @@ import { getStoredToken } from "../../../lib/token.js";
 import prisma from "../../../lib/db.js";
 import { select } from "@clack/prompts";
 import { startChat } from "../../chat/chat-with-ai.js";
+import { startToolChat } from "../../chat/chat-with-ai-tools.js";
 
 const wakeUpAction = async () => {
   const token = await getStoredToken();
@@ -70,7 +71,7 @@ const wakeUpAction = async () => {
       break;
     case "tool":
       // console.log("tool is selected");
-      // await startToolChat();
+      await startToolChat();
       break;
     case "agent":
       // console.log(" agent Chat is selected");
